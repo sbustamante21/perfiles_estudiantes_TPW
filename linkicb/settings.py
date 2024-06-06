692241a1cb814e640fa6b1c39ba04f38c0d061f0
seba <sebastianbusta_villarreal@hotmail.com>
2024-06-06 15:13:27 -0400

    funcionalidad del login

--- linkicb/settings.py
+++ linkicb/settings.py
@@ -79,11 +79,21 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-# TEMPLATES = [
-# { 
-#   'DIRS': [os.path.join(BASE_DIR, 'templates/')]}
-# ]
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates' ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
--- website/views.py
+++ website/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
- 
+from django.contrib.auth import views as auth_views
+
 # Views
 @login_required
 def home(request):
-    return render(request, "registration/success.html", {})
+    return render(request, "website/success.html", {})
+
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'website/login.html'
  
 def register(request):
     if request.method == 'POST':
@@ -20,5 +24,5 @@ def register(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
 # Create your views here.
