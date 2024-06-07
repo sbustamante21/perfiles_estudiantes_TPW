2bd80936b6c27926ed897a31af7d6dd968a3654b
sebastian122342 <sebastianbusta_villarreal@hotmail.com>
2024-06-07 09:13:43 -0400

    validaciones generales

--- website/forms.py
+++ website/forms.py
@@ -5,22 +5,32 @@ from .models import Profile, Student, CurriculumPlan, Degree
 from django.core.validators import MaxValueValidator
 import datetime
 
+
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name','password1', 'password2',]
-    
+        fields = [
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        ]
+
     def clean_email(self):
-        email = self.cleaned_data.get('email')
+        email = self.cleaned_data.get("email")
 
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('This value already exists.')
-        if not email.endswith("@alumnos.utalca.cl") and not email.endswith("@utalca.cl"):
+            raise forms.ValidationError("This value already exists.")
+        if not email.endswith("@alumnos.utalca.cl") and not email.endswith(
+            "@utalca.cl"
+        ):
             raise forms.ValidationError("You must use your institution's e-mail.")
         return email
 
@@ -30,39 +40,49 @@ class ProfileRegisterForm(forms.ModelForm):
         model = Profile
         exclude = ["role_id", "user"]
 
+
 class StudentRegisterForm(forms.ModelForm):
     admission_year = forms.IntegerField(required=True)
     personal_mail = forms.EmailField(required=False)
     phone_number = forms.IntegerField(required=False)
-    curriculum_plan_id = forms.ModelChoiceField(queryset=CurriculumPlan.objects.all(), required=True)
+    curriculum_plan_id = forms.ModelChoiceField(
+        queryset=CurriculumPlan.objects.all(), required=True
+    )
     degree_id = forms.ModelChoiceField(queryset=Degree.objects.all(), required=True)
 
     class Meta:
         model = Student
-        fields = ['admission_year', 'personal_mail', 'phone_number', 'pfp', 'degree_id', 'curriculum_plan_id',]
+        fields = [
+            "admission_year",
+            "personal_mail",
+            "phone_number",
+            "pfp",
+            "degree_id",
+            "curriculum_plan_id",
+        ]
         exclude = ["pfp"]
 
     def clean_admission_year(self):
-        admission_year = self.cleaned_data.get('admission_year')
+        admission_year = self.cleaned_data.get("admission_year")
 
         if not datetime.datetime.now().year >= admission_year >= 1980:
-            raise forms.ValidationError('Year out of bounds')
+            raise forms.ValidationError("Year out of bounds")
         return admission_year
-    
+
     def clean_personal_mail(self):
-        email = self.cleaned_data.get('personal_mail')
+        email = self.cleaned_data.get("personal_mail")
 
         if email != "":
             if Student.objects.filter(personal_mail=email).exists():
-                raise forms.ValidationError('This email is already used')
+                raise forms.ValidationError("This email is already used")
         return email
-    
-    def clean_phone_number(self):
-        number = self.cleaned_data.get('phone_number')
 
-        if number != "":
+    def clean_phone_number(self):
+        number = self.cleaned_data.get("phone_number")
+
+        if number is not None:
             if Student.objects.filter(phone_number=number).exists():
-                raise forms.ValidationError('This phone number is already used')
-            elif len(str(num)) != 9:
-                raise forms.ValidationError('The phone number must be 9 digits long')
-        return email
+                raise forms.ValidationError("This phone number is already used")
+            elif len(str(number)) != 9:
+                raise forms.ValidationError("The phone number must be 9 digits long")
+        return number
--- website/views.py
+++ website/views.py
@@ -9,94 +9,111 @@ from django.contrib.auth import views as auth_views
 from .forms import StudentRegisterForm, UserRegisterForm, ProfileRegisterForm
 from .models import Student, Role, Profile, CurriculumPlan
 from django.contrib.auth.views import LogoutView
-from django.urls import reverse_lazy
-
 
 # Create your views here.
+
 
 # Views
 @login_required
 def home(request):
     return render(request, "website/success.html", {})
 
+
 def do_logout(request):
     if request.user.is_authenticated:
-        logout(request) 
+        logout(request)
     return redirect("inicio")
 
+
 class CustomLoginView(auth_views.LoginView):
-    template_name = 'website/login.html'
+    template_name = "website/login.html"
+
 
 def inicio(request):
-    return render(request, 'website/inicio.html')
+    return render(request, "website/inicio.html")
+
 
 def login(request):
-    return render(request, 'website/login.html')
+    return render(request, "website/login.html")
+
 
 def register(request):
-    return render(request, 'website/register.html')
+    return render(request, "website/register.html")
+
 
 def student_register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = UserRegisterForm(request.POST)
         profile_form = ProfileRegisterForm(request.POST)
         student_form = StudentRegisterForm(request.POST, request.FILES)
-        
+
         if user_form.is_valid() and profile_form.is_valid() and student_form.is_valid():
             user = user_form.save(commit=False)
-            user.set_password(user_form.cleaned_data['password1'])
+            user.set_password(user_form.cleaned_data["password1"])
             user.save()
-            
+
             profile = profile_form.save(commit=False)
             profile.user = user
 
-            default_role = Role.objects.get(name='ESTUDIANTE')  # Change 'Student' to the appropriate role name
+            default_role = Role.objects.get(
+                name="ESTUDIANTE"
+            )  # Change 'Student' to the appropriate role name
             profile.role_id = default_role
             profile.save()
-            
+
             student = student_form.save(commit=False)
             student.user_id = profile
             student.save()
-            
+
             # Redirect to the login page
-            return redirect(reverse('inicio'))
+            return redirect(reverse("inicio"))
 
     else:
         user_form = UserRegisterForm()
         profile_form = ProfileRegisterForm()
         student_form = StudentRegisterForm()
-        
-    return render(request, 'website/student_register.html', {
-        'user_form': user_form,
-        'profile_form': profile_form,
-        'student_form': student_form
-    })
+
+    return render(
+        request,
+        "website/student_register.html",
+        {
+            "user_form": user_form,
+            "profile_form": profile_form,
+            "student_form": student_form,
+        },
+    )
+
 
 def professor_register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = UserRegisterForm(request.POST)
         profile_form = ProfileRegisterForm(request.POST)
-        
+
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save(commit=False)
-            user.set_password(user_form.cleaned_data['password1'])
+            user.set_password(user_form.cleaned_data["password1"])
             user.save()
-            
+
             profile = profile_form.save(commit=False)
             profile.user = user
-            professor_role = Role.objects.get(name='DOCENTE')
+            professor_role = Role.objects.get(name="DOCENTE")
             profile.role_id = professor_role
             profile.save()
-            
-            return redirect(reverse('inicio'))
+
+            return redirect(reverse("inicio"))
     else:
         user_form = UserRegisterForm()
         profile_form = ProfileRegisterForm()
-        
-    return render(request, 'website/professor_register.html', {
-        'user_form': user_form,
-        'profile_form': profile_form,
-    })
+
+    return render(
+        request,
+        "website/professor_register.html",
+        {
+            "user_form": user_form,
+            "profile_form": profile_form,
+        },
+    )
+
 
 def login(request):
-    return render(request, 'website/login.html')
+    return render(request, "website/login.html")
