ec9ec9e58ae0e633f11d90402ff3b6020e5dc514
seba <sebastianbusta_villarreal@hotmail.com>
2024-07-03 21:38:23 -0400

    responsividad en tabla de trabajos pasados

--- linkicb/settings.py
+++ linkicb/settings.py
@@ -140,6 +140,7 @@ USE_I18N = True
 USE_TZ = True
 
 USE_DJANGO_JQUERY = True
+JQUERY_URL = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
