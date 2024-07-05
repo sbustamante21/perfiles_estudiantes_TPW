d921e7899276ff14958edb0292796b563c18f1cf
sebastian122342 <sebastianbusta_villarreal@hotmail.com>
2024-07-05 00:25:48 -0400

    mas estetica, mantenedores de admin con table responsive

--- static/website/main.js
+++ static/website/main.js
@@ -1,5 +1,3 @@
 function setReceiverId(userId) {
     document.getElementById('receiver_in_modal').value = userId;
 }
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
